fff28df45d47f8dc881b618e726953b1e1d97b2c
Grzegorz Gackowski <greggupl1@gmail.com>
2019-12-09 09:26:34 +0000

    Delete FilteringAuthor.java

--- src/ggbibtex/FilteringAuthor.java
+++
@@ -1,9 +0,0 @@
-package ggbibtex;
-
-import java.util.HashMap;
-
-public class FilteringAuthor implements IFiltering {
-    public HashMap<String, Record> filter(HashMap<String, Record> records, String constr) {
-        return records;
-    }
-}
